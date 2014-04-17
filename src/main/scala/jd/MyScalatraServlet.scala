47ac8e29dbfddac037b8e5e595a4ce5d36e8f7b4
marvin <marvin@PC.(none)>
2014-04-17 10:57:55 +0800

    view

--- src/main/scala/jd/MyScalatraServlet.scala
+++ src/main/scala/jd/MyScalatraServlet.scala
@@ -7,8 +7,12 @@ import scala.xml.Node
 class MyScalatraServlet extends JdStack {
 
   get("/") {
-      <a href="/t">p</a>
-    }
-
+//一定记得定义文件类型
+    contentType="text/html"
+    val kk:String = "<p>first blood</p>"
+//指定view模板和layout，将值赋给view
+//    jade("empty.jade","layout"->"WEB-INF/templates/layouts/default.jade","aa"->kk)
+    jade("empty.jade","aa"->kk)
+  }
   
 }
