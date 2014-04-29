818bdcc0fc5824473920d25a63a72b28546ea8bd
marvin <marvin@PC.(none)>
2014-04-29 12:04:48 +0800

    index edit menu

--- src/main/scala/jd/MyScalatraServlet.scala
+++ src/main/scala/jd/MyScalatraServlet.scala
@@ -22,33 +22,54 @@ class MyScalatraServlet(mongoColl:MongoCollection) extends JdStack {
 
     
   post("/edit"){
+
+    contentType="text/html"
     val poemTitle = params("poemTitle")
     val poemContent = params("poemContent")
     val newObj = MongoDBObject("poemTitle" -> poemTitle,"poemContent"->poemContent)
     mongoColl.insert(newObj)
+    redirect("/query/null")
   }
+  
+  get("/edit"){
+    contentType="text/html"
+    jade("edit.jade")
+  }
+
   
   get("/") {
 //一定记得定义文件类型
     contentType="text/html"
-    val kk:String = "<p>first blood</p>"
+    val face:String = "Welcome"
+    jade("index.jade","face"->face)
 //指定view模板和layout，将值赋给view
 //    jade("empty.jade","layout"->"WEB-INF/templates/layouts/default.jade","aa"->kk)
-    jade("empty.jade","aa"->kk)
+
   }
 
 
-  get("/:key"){
+  get("/query/:key"){
         contentType = "text/html"
         var list = new ListBuffer[List[String]]
         val allDoc = mongoColl.find()  
+        var pTitle:String = ""
+        var pId:String = ""
+        var poemContent = ""
+        var poemTitle = ""
         for (i <- allDoc){
-          val lt = List(i.getOrElse("poemTitle", "No answer").toString(),
-                                    i.getOrElse("poemContent", "No answer").toString(),
-                                    i.getAs[ObjectId]("_id") map (_.toString) getOrElse "No answer")
+          pId = i.getAs[ObjectId]("_id") map (_.toString) getOrElse "No answer"
+          pTitle = i.getOrElse("poemTitle","No answer").toString()
+          val lt = List(pId,pTitle)
           list += lt
+          if(pId == params("key").toString()){
+            poemContent = i.getOrElse("poemContent","No answer").toString()
+            poemTitle = pTitle
+          }
+
+          
         }
-        jade("content.jade","list" -> list.toList);
+
+        jade("content.jade","list" -> list.toList,"poemContent" -> poemContent,"poemTitle" -> poemTitle);
   }
   
 }
